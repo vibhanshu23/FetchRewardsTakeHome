94a1513504787db752220607bbabdf3f48e26413
vibhanshu23 <vibhanshu7ain@gmail.com>
2023-04-04 00:47:35 -0500

    Ignore.

--- FetchRewardsTakeHome/ViewController.swift
+++ FetchRewardsTakeHome/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 
+        
         let viewModel = MealViewModel()
         viewModel.getMealList { arrMeals in
             for item in arrMeals{
